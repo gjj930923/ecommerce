daf8f249de072dd3b459e7729384d42cd1f553a6
HZJTEH <zhh340324@gmail.com>
2016-11-22 16:43:41 -0500

    HZJ

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,18 @@
 <?php
 namespace app\index\controller;
-
-class Index
+use think\Controller;
+class Index extends Controller
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">åå¹´ç£¨ä¸€å‰‘ - ä¸ºAPIå¼€å‘è®¾è®¡çš„é«˜æ€§èƒ½æ¡†æ¶</span></p><span style="font-size:22px;">[ V5.0 ç‰ˆæœ¬ç”± <a href="http://www.qiniu.com" target="qiniu">ä¸ƒç‰›äº‘</a> ç‹¬å®¶èµåŠ©å‘å¸ƒ ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">Ê®ÄêÄ¥Ò»½£ - ÎªAPI¿ª·¢Éè¼ÆµÄ¸ßĞÔÄÜ¿ò¼Ü</span></p><span style="font-size:22px;">[ V5.0 °æ±¾ÓÉ <a href="http://www.qiniu.com" target="qiniu">ÆßÅ£ÔÆ</a> ¶À¼ÒÔŞÖú·¢²¼ ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+    }
+    public function hello()
+   {
+	return $this->fetch();
+    }
+    public function login()
+   {
+       return $this->fetch();
     }
 }
